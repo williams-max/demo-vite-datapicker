d6796206a24db06d9026d762724ba2881b2ccdcc
Israel <almanzaisrael75@gmail.com>
2023-04-20 17:49:11 -0400

    test proyect clean vite 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+
 
 
 function App() {
@@ -24,6 +31,31 @@ function App() {
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DemoContainer components={['DatePicker']}>
           <DatePicker label="Basic date picker" />
+        </DemoContainer>
+      </LocalizationProvider>
+
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer
+          components={[
+            'DatePicker',
+            'MobileDatePicker',
+            'DesktopDatePicker',
+            'StaticDatePicker',
+          ]}
+        >
+          <DemoItem label="Desktop variant">
+            <DesktopDatePicker defaultValue={dayjs('2022-04-17')} />
+          </DemoItem>
+          <DemoItem label="Mobile variant">
+            <MobileDatePicker defaultValue={dayjs('2022-04-17')} />
+          </DemoItem>
+          <DemoItem label="Responsive variant">
+            <DatePicker defaultValue={dayjs('2022-04-17')} />
+          </DemoItem>
+          <DemoItem label="Static variant">
+            <StaticDatePicker defaultValue={dayjs('2022-04-17')} />
+          </DemoItem>
         </DemoContainer>
       </LocalizationProvider>
     </>
